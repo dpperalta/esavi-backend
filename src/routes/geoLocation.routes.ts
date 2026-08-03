132bc7cff0a61b10f332e0fe7bbf31f284cc87ac
dpperalta <die_go_p@hotmail.com>
2026-08-03 09:09:46 -0500

    PRESPEC06-Stem completed - 7

--- src/routes/geoLocation.routes.ts
+++ src/routes/geoLocation.routes.ts
@@ -6,30 +6,30 @@ import { activateGeoLocation, createGeoLocation, deleteGeoLocation, getGeoLocati
 
 const { SUPERADMIN, ADMIN, USER } = ROLES;
 
-const route = Router();
+const router = Router();
 
 // Create Geographic Location
 // Code: ESAVI-GEOLOC-001
-route.post('/', tokenValidation, validateUserRole(ADMIN), ...createGeoLocationValidator, validateFields, createGeoLocation);
+router.post('/', tokenValidation, validateUserRole(ADMIN), ...createGeoLocationValidator, validateFields, createGeoLocation);
 
 // Get Geographic Locations
 // Code: ESAVI-GEOLOC-002
-route.get('/', tokenValidation, validateUserRole(USER), ...geoLocationListValidator, validateFields, getGeoLocations);
+router.get('/', tokenValidation, validateUserRole(USER), ...geoLocationListValidator, validateFields, getGeoLocations);
 
 // Get Geographic Location by ID
 // Code: ESAVI-GEOLOC-003
-route.get('/:id', tokenValidation, validateUserRole(USER), ...geoLocationIdValidator, validateFields, getGeoLocationById);
+router.get('/:id', tokenValidation, validateUserRole(USER), ...geoLocationIdValidator, validateFields, getGeoLocationById);
 
 // Update Geographic Location
 // Code: ESAVI-GEOLOC-004
-route.put('/:id', tokenValidation, validateUserRole(ADMIN), ...geoLocationIdValidator, ...updateGeoLocationValidator, validateFields, updateGeoLocation);
+router.put('/:id', tokenValidation, validateUserRole(ADMIN), ...geoLocationIdValidator, ...updateGeoLocationValidator, validateFields, updateGeoLocation);
 
 // Soft delete Geographic Location
 // Code: ESAVI-GEOLOC-005A
-route.delete('/:id', tokenValidation, validateUserRole(ADMIN), ...geoLocationIdValidator, validateFields, deleteGeoLocation);
+router.delete('/:id', tokenValidation, validateUserRole(ADMIN), ...geoLocationIdValidator, validateFields, deleteGeoLocation);
 
 // Activate Geographic Location
 // Code: ESAVI-GEOLOC-005B
-route.patch('/activate/:id', tokenValidation, validateUserRole(SUPERADMIN), ...geoLocationIdValidator, validateFields, activateGeoLocation);
+router.patch('/activate/:id', tokenValidation, validateUserRole(SUPERADMIN), ...geoLocationIdValidator, validateFields, activateGeoLocation);
 
-export default route;
+export default router;
